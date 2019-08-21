dfef801425018ff972ca17fffb2d9e40ee0920ff
Ferdinand Langer <s7felang@uni-bonn.de>
2019-08-21 15:43:26 +0200

    Fix memory leak

--- auxiliary/raytracer/RayTracer.cpp
+++ auxiliary/raytracer/RayTracer.cpp
@@ -51,11 +51,11 @@ void trace(float* rays, float* origin_in, float* verts, int* faces, int* colors,
 
   Vector3 origin(origin_in[0], origin_in[1], origin_in[2]);
 
-  printf("Rendering image (%dx%d)...\n", width, height);
+  printf("Rendering image (%dx%d)...\n", height, width);
   // Raytrace over every pixel
 #pragma omp parallel for
   for(size_t i=0; i<width; ++i) {
-    for(size_t j=0; j<height; ++j) {
+    for(int j=0; j<height; ++j) {
       size_t index = 3*(width * j + i);
 
       Vector3 single_ray(rays[index+0], rays[index+1], rays[index+2]);
@@ -104,95 +104,15 @@ void trace(float* rays, float* origin_in, float* verts, int* faces, int* colors,
   // }
   // fclose(image);
 
-  // Cleanup
-  // delete[] pixels;
+  // loop over object vector delete 
+  for (Object* p : objects) {
+     delete p;
 }
-
-void demo() {
-   // Create a million triangles packed in the space of a cube
-  const unsigned int N = 10000;
-  vector<Object*> objects;
-  printf("Constructing %d triangles...\n", N);
-  for(size_t i=0; i<N; ++i) {
-    objects.push_back(new Triangle(randVector3(), randVector3(), randVector3(), randVector3(), randVector3(), randVector3()));
-  // objects.push_back(new Triangle(Vector3(0,0,1), Vector3(0,0,0), Vector3(1,0,0), Vector3(1,0,0), Vector3(1,0,0), Vector3(1,0,0)));
-  // objects.push_back(new Triangle(Vector3(0,0,-1), Vector3(0,0,0), Vector3(-1,0,0), Vector3(0,1,0), Vector3(0,1,0), Vector3(0,1,0)));
-  }
-
-  // Compute a BVH for this object set
-  BVH bvh(&objects);
-
-  // Allocate space for some image pixels
-  const unsigned int width=800, height=64;
-  float* pixels = new float[width*height*3];
-
-  // Create a camera from position and focus point
-  Vector3 origin(1.6, 1.3, 1.6);
-  Vector3 camera_focus(0,0,0);
-  Vector3 camera_up(0,1,0);
-
-  // Camera tangent space
-  Vector3 camera_dir = normalize(camera_focus - origin);
-  Vector3 camera_u = normalize(camera_dir ^ camera_up);
-  Vector3 camera_v = normalize(camera_u ^ camera_dir);
-
-  printf("Rendering image (%dx%d)...\n", width, height);
-  // Raytrace over every pixel
-#pragma omp parallel for
-  for(size_t i=0; i<width; ++i) {
-    for(size_t j=0; j<height; ++j) {
-      size_t index = 3*(width * j + i);
-
-      float u = (i+.5f) / (float)(width-1) - .5f;
-      float v = (height-1-j+.5f) / (float)(height-1) - .5f;
-      float fov = .5f / tanf( 70.f * 3.14159265*.5f / 180.f);
-
-      // This is only valid for square aspect ratio images
-      Ray ray(origin, normalize(u*camera_u + v*camera_v + fov*camera_dir));
-
-      IntersectionInfo I;
-      bool hit = bvh.getIntersection(ray, &I, false);
-
-      if(!hit) {
-        pixels[index] = pixels[index+1] = pixels[index+2] = 0.f;
-      } else {
-        // const Vector3 color(fabs(c0.x), fabs(c0.y), fabs(c0.z));
-        // const Vector3 normal = I.object->getNormal(I);
-        Vector3 colors = I.object->getColor(0);
-        // const Vector3 color(fabs(normal.x), fabs(normal.y), fabs(normal.z));
-
-        pixels[index+0] = colors.x;
-        // pixels[index+0] = 1;
-        pixels[index+1] = colors.y;
-        // pixels[index+1] = 1;
-        pixels[index+2] = colors.z;
-        // pixels[index+2] = 1;
-      }
-    }
-  }
-
-  // Output image file (PPM Format)
-  printf("Writing out image file: \"render.ppm\"\n");
-  FILE *image = fopen("render.ppm", "w");
-  fprintf(image, "P6\n%d %d\n255\n", width, height);
-  for(size_t j=0; j<height; ++j) {
-    for(size_t i=0; i<width; ++i) {
-      size_t index = 3*(width * j + i);
-      unsigned char r = std::max(std::min(pixels[index+0]*255.f, 255.f), 0.f);
-      unsigned char g = std::max(std::min(pixels[index+1]*255.f, 255.f), 0.f);
-      unsigned char b = std::max(std::min(pixels[index+2]*255.f, 255.f), 0.f);
-      fprintf(image, "%c%c%c", r,g,b);
-    }
-  }
-  fclose(image);
-
-  // Cleanup
-  delete[] pixels;
+   objects.clear();
 }
 
 extern "C"
 {
-  void cdemo() {demo(); printf("Demo done\n");}
   void ctrace(float* rays, float* origin, float* verts, int* faces, int* colors,
               int n_rays, int n_verts, int n_faces, int height,
               float* endpoints, int* endcolors, float* range) {
--- auxiliary/raytracer/setup.py
+++ auxiliary/raytracer/setup.py
@@ -9,7 +9,7 @@ print(numpy.get_include())
 ext = Extension('RayTracerCython', sources=["RayTracerCython.pyx"],
                                    language="c++",
                                    include_dirs=[numpy.get_include(), '.'],
-                                   extra_compile_args=['-O3', '-fopenmp', '-msse3', '-fPIC', '-march=native'],
+                                   extra_compile_args=["-std=c++14", '-O3', '-fopenmp', '-msse3', '-fPIC', '-march=native'],
                                    extra_link_args=['-lgomp'],
                                    )
 
