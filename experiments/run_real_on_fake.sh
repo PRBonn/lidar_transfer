32134f6442c5142578deacb9b1217dc2182d212a
Ferdinand Langer <s7felang@uni-bonn.de>
2019-08-27 12:17:56 +0200

    Absolute paths in scripts

--- experiments/run_real_on_fake.sh
+++ experiments/run_real_on_fake.sh
@@ -9,18 +9,21 @@ PRETRAINED="~/workspace/msc/lidar-bonnetal/train/models/darknet53/"
 PREDICTION="/media/flanger/SAMS1TB_0/msc/experiments/$EXP/predictions/$TYPE"
 MODEL="/media/flanger/SAMS1TB_0/msc/experiments/$EXP/logs/$VERSION"
 DATA="/media/flanger/SAMS1TB_0/msc/experiments/$EXP/dataset/"
-cd ~/workspace/msc
+source ~/workspace/msc/venv/bin/activate
+
 # Training
 # use pretrained model on real data
 
 # Infer
-./lidar-bonnetal/train/tasks/semantic/infer.py \
+cd /automount_home_students/flanger/workspace/msc/lidar-bonnetal/train/tasks/semantic/
+./infer.py \
     -d "$DATA" \
     -m "$MODEL" \
     -l "$PREDICTION"
 
 # Evaluate
-./semantic-kitti-api/evaluate_semantics.py \
+cd /automount_home_students/flanger/workspace/msc/semantic-kitti-api/
+./evaluate_semantics.py \
     -d "$DATA" \
     -p "$PREDICTION" \
     -s valid
