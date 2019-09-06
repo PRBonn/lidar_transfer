e87e87eb3e46298407a11075aac79600cd9af6dd
Ferdinand Langer <s7felang@uni-bonn.de>
2019-09-06 13:58:00 +0200

    Skip openmp dependency on macos

--- auxiliary/raytracer/setup.py
+++ auxiliary/raytracer/setup.py
@@ -3,15 +3,21 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy
+from sys import platform
 
 print(numpy.get_include())
 
-ext = Extension('RayTracerCython', sources=["RayTracerCython.pyx"],
-                                   language="c++",
-                                   include_dirs=[numpy.get_include(), '.'],
-                                   extra_compile_args=["-std=c++14", '-O3', '-fopenmp', '-msse3', '-fPIC', '-march=native'],
-                                   extra_link_args=['-lgomp'],
-                                   )
+if platform == "darwin":
+  ext = Extension('RayTracerCython', sources=["RayTracerCython.pyx"],
+                  language="c++",
+                  include_dirs=[numpy.get_include(), '.'],
+                  extra_compile_args=["-std=c++14", '-O3', '-msse3', '-fPIC', '-march=native'])
+else:
+  ext = Extension('RayTracerCython', sources=["RayTracerCython.pyx"],
+                  language="c++",
+                  include_dirs=[numpy.get_include(), '.'],
+                  extra_compile_args=["-std=c++14", '-O3', '-fopenmp', '-msse3', '-fPIC', '-march=native'],
+                  extra_link_args=['-lgomp'],)
 
 setup(name="RayTracerCython",
       ext_modules = cythonize(ext, language_level="3"))
